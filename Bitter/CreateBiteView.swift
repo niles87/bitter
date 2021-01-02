c3903ea48dea4a95b173d8f760a4d7e48dddfca1
niles87 <nbing19@gmail.com>
2021-01-02 12:32:25 -0800

    basic new bite entry layout

--- Bitter/CreateBiteView.swift
+++ Bitter/CreateBiteView.swift
@@ -9,12 +9,48 @@ import SwiftUI
 
 struct CreateBiteView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("lightgray").ignoresSafeArea()
+            VStack {
+                TitleBar()
+                
+                InputBite()
+                Spacer()
+                ButtonView()
+            }
+        }
     }
 }
 
 struct CreateBiteView_Previews: PreviewProvider {
     static var previews: some View {
         CreateBiteView()
+    }
+}
+
+struct InputBite: View {
+    @State var biteEntry: String = "Enter a new bite!"
+    
+    var body: some View {
+        VStack {
+            Text("New Bite").padding()
+                .font(.system(size: 20.0, weight: .semibold))
+                .foregroundColor(Color("username"))
+            TextEditor(text: $biteEntry)
+            HStack {
+                Spacer()
+                Button(action: {
+                    print("add new bite")
+                }, label: {
+                    Text("Submit").foregroundColor(.white)
+                }).frame(width: 75, height: 40)
+                .background(Color("button"))
+                .cornerRadius(10.0)
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 5))
+            }
+            
+        }.frame(width: UIScreen.main.bounds.width - 30, height: 300)
+        .border(Color.secondary, width: 3)
+        .cornerRadius(5.0)
     }
 }
