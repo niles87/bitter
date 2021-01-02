0f2a7a9d957950c26f1293afc2c273ce78805649
niles87 <nbing19@gmail.com>
2021-01-01 23:06:17 -0800

    added UI components

--- Bitter/ContentView.swift
+++ Bitter/ContentView.swift
@@ -9,17 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         ZStack {
+            Color("lightgray").ignoresSafeArea()
             
-            VStack{
-                Text("Bitter")
-                    .padding()
-                    .font(.system(size: 25.0, weight: .semibold))
-                    .foregroundColor(.green)
-                
-                
+            VStack {
+                HStack {
+                    Text("Bitter")
+                        .padding(EdgeInsets(top: 0.0, leading: 10.0, bottom: 10.0, trailing: 10.0))
+                        .font(.system(size: 30.0, weight: .semibold))
+                        .foregroundColor(Color("username"))
+                        .frame(width: UIScreen.main.bounds.width - 55, height: 50, alignment: .leading)
+                    Button(action: {
+                        print("sign in")
+                    }, label: {
+                        Image(systemName: "person.crop.circle.badge.xmark").resizable()
+                            .frame(width: 45, height: 40)
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(Color("username"))
+                    })
+                }
+                    
+                VStack { // BiteView will go here once DB is connected
+                    
+                    HStack {
+                        Image(systemName: "person.circle").resizable().frame(width: 40, height: 40)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.trailing)
+                        
+                        Text("Username").font(.system(size: 18.0, weight: .medium)).padding(.leading)
+                    }
+                    
+                    VStack {
+                        Text("This is the first Bite ever!!!!!").padding()
+                    }
+                }
+                Spacer()
+                ButtonView()
             }
-            
         }
     }
 }
@@ -31,11 +58,55 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
-struct BiteView {
+struct BiteView: View {
+    
+    var userImage: String
+    var userName: String
+    var bite: String
     
     var body: some View {
-        HStack {
+        VStack {
             
+            HStack {
+                Image(systemName: userImage).resizable().frame(width: 40, height: 40)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.trailing)
+                
+                Text(userName).font(.system(size: 18.0, weight: .medium)).padding(.leading)
+                    .foregroundColor(Color("username"))
+            }
+            
+            VStack {
+                Text(bite).padding()
+            }
+        }
+    }
+}
+
+struct ButtonView: View {
+    
+    var body: some View {
+        HStack{
+            Button(action: {
+                print("search button")
+            }, label: {
+                Text("Search")
+            }).frame(width: UIScreen.main.bounds.width / 3, height: 50).background(Color("button"))
+                .foregroundColor(.black)
+                .cornerRadius(5)
+            
+            Button(action: {
+                print("Create Bite")
+            }, label: {
+                Image(systemName: "plus.circle.fill").resizable().aspectRatio(contentMode: .fit)
+            }).frame(width: 50, height: 50).cornerRadius(50)
+            
+            Button(action: {
+                print("profile")
+            }, label: {
+                Text("Profile")
+            }).frame(width: UIScreen.main.bounds.width / 3, height: 50).background(Color("button"))
+            .cornerRadius(5).foregroundColor(.black)
         }
     }
 }
