de23a3d49f6243975059114ccc607e71c8a8ce9d
niles87 <nbing19@gmail.com>
2021-01-02 12:30:41 -0800

    updated home screen layout moved reusable components into different files

--- Bitter/ContentView.swift
+++ Bitter/ContentView.swift
@@ -14,21 +14,7 @@ struct ContentView: View {
             Color("lightgray").ignoresSafeArea()
             
             VStack {
-                HStack {
-                    Text("Bitter")
-                        .padding(EdgeInsets(top: 0.0, leading: 10.0, bottom: 10.0, trailing: 10.0))
-                        .font(.system(size: 30.0, weight: .semibold))
-                        .foregroundColor(Color("username"))
-                        .frame(width: UIScreen.main.bounds.width - 55, height: 50, alignment: .leading)
-                    Button(action: {
-                        print("sign in")
-                    }, label: {
-                        Image(systemName: "person.crop.circle.badge.xmark").resizable()
-                            .frame(width: 45, height: 40)
-                            .aspectRatio(contentMode: .fit)
-                            .foregroundColor(Color("username"))
-                    })
-                }
+                TitleBar()
                     
                 VStack { // BiteView will go here once DB is connected
                     
@@ -58,55 +44,5 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
-struct BiteView: View {
-    
-    var userImage: String
-    var userName: String
-    var bite: String
-    
-    var body: some View {
-        VStack {
-            
-            HStack {
-                Image(systemName: userImage).resizable().frame(width: 40, height: 40)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.trailing)
-                
-                Text(userName).font(.system(size: 18.0, weight: .medium)).padding(.leading)
-                    .foregroundColor(Color("username"))
-            }
-            
-            VStack {
-                Text(bite).padding()
-            }
-        }
-    }
-}
 
-struct ButtonView: View {
-    
-    var body: some View {
-        HStack{
-            Button(action: {
-                print("search button")
-            }, label: {
-                Text("Search")
-            }).frame(width: UIScreen.main.bounds.width / 3, height: 50).background(Color("button"))
-                .foregroundColor(.black)
-                .cornerRadius(5)
-            
-            Button(action: {
-                print("Create Bite")
-            }, label: {
-                Image(systemName: "plus.circle.fill").resizable().aspectRatio(contentMode: .fit)
-            }).frame(width: 50, height: 50).cornerRadius(50)
-            
-            Button(action: {
-                print("profile")
-            }, label: {
-                Text("Profile")
-            }).frame(width: UIScreen.main.bounds.width / 3, height: 50).background(Color("button"))
-            .cornerRadius(5).foregroundColor(.black)
-        }
-    }
-}
+
